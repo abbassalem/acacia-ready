eeb426af6f485f174df3da30564bf9ea3defdbec
Abbas Salem <abbas.salem.water@gmail.com>
2022-06-04 22:22:46 +0100

    upgarde to angular14 material14

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+// import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
